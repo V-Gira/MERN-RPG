cb81ffef3d7b3a0ca308b7b1cb799abfa2dd114c
Virgile <virgile@manjaro.fritz.box>
2021-02-08 17:23:09 +0100

    create character.model.js

---
+++ server/models/character.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const CharacterSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Name is required'
+  },
+  image: {
+    data: Buffer,
+    contentType: String
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  system: {
+    type: String,
+    required: 'System is required'
+  },
+  updated: Date,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  player: {type: mongoose.Schema.ObjectId, ref: 'User'}
+})
+
+export default mongoose.model('Character', GameSchema)
